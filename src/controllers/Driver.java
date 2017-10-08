d8fa35ba178618537aec9aaf40939068675c0ed9
Ayaan Kazerouni <ayaan@vt.edu>
2017-10-07 21:39:53 -0400

    Use bleeding edge repodriller.

--- src/controllers/Driver.java
+++ src/controllers/Driver.java
@@ -5,9 +5,9 @@ import org.repodriller.RepoDriller;
 public class Driver {
 
 	public static void main(String[] args) {
-		String singleRepoPath = "~/Developer/repos/17606_P1".replaceFirst("^~", System.getProperty("user.home"));
-		String aggregateRepoPath = "~/Developer/repos/".replaceFirst("^~", System.getProperty("user.home"));
-//		new RepoDriller().start(new SingleRepoStudy(singleRepoPath, "/tmp/singleRepo.csv"));
-		new RepoDriller().start(new AggregateRepoStudy(aggregateRepoPath, "/tmp/aggregateRepo.csv"));
+		String singleRepoPath = "~/Developer/repos/10116_P4".replaceFirst("^~", System.getProperty("user.home"));
+//		String aggregateRepoPath = "~/Developer/repos/".replaceFirst("^~", System.getProperty("user.home"));
+		new RepoDriller().start(new SingleRepoStudy(singleRepoPath, "/tmp/singleRepo.csv"));
+//		new RepoDriller().start(new AggregateRepoStudy(aggregateRepoPath, "/tmp/aggregateRepo.csv"));
 	}
 }
