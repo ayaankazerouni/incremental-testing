f303f4598006093e26db20ba6e70c1f4fff60dc9
Ayaan Kazerouni <ayaan@vt.edu>
2017-10-25 17:47:17 -0400

    Omit methods that were removed before the end.

--- src/controllers/Driver.java
+++ src/controllers/Driver.java
@@ -5,7 +5,7 @@ import org.repodriller.RepoDriller;
 public class Driver {
 
 	public static void main(String[] args) {
-		String repoPath = System.getProperty("user.dir") + "/student-repos/repos/b02ceb15-b340-4130-b09b-96bd16dfb8a1_tonaid_Project3/";
-		new RepoDriller().start(new SensorDataStudy(repoPath, "/tmp/repo-mining.csv", true));
+		String repoPath = System.getProperty("user.dir") + "/student-repos/repos/";
+		new RepoDriller().start(new SensorDataStudy(repoPath, "/tmp/repo-mining.csv", false));
 	}
 }
