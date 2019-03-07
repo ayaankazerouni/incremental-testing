c645b87c5271ad24f0c5c210a2215a7457e01cc1
Ayaan Kazerouni <ayaan@vt.edu>
2019-03-07 13:15:29 -0500

    Get ready to package

--- src/controllers/Driver.java
+++ src/controllers/Driver.java
@@ -6,9 +6,8 @@ import org.repodriller.RepoDriller;
 public class Driver {
 
 	public static void main(String[] args) {
-		String repoPath = System.getProperty("user.home") + "/Developer/student-projects/testrepos/10d98918-7bee-4cb8-9316-7647a1947077_chrsrck_Project3";
-		new RepoDriller().start(new SensorDataStudy(repoPath, "/tmp/test-creation.csv", true));
-//		new RepoDriller().start(new CoEvolutionStudy(repoPath, "/tmp/coevolution.csv", true));
-//		new RepoDriller().start(new MethodModificationStudy(repoPath, "/tmp/sample-event.csv", true));
+		String repoPath = args[0];
+		boolean single = Boolean.parseBoolean(args[1]);
+		new RepoDriller().start(new MethodModificationStudy(repoPath, "/tmp/method-modifications.csv", single));
 	}
 }
